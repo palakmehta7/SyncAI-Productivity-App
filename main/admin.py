b35608432a9b91de78e646581070a155a00bbdba
Palak Mehta <palak.mehta@haptik.co>
2024-10-26 01:18:54 +0530

    Added model changes

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Project, Task, Asignee
+
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Asignee)
--- main/models.py
+++ main/models.py
@@ -9,18 +9,24 @@ STATUSES = (
 
 class Project(models.Model):
     desc = models.CharField(max_length=200)
-    start_date = models.DateTimeField("date published")
+    created_at = models.DateTimeField("date created")
+    updated_at = models.DateTimeField("date updated")
     summary = models.CharField(max_length=10)
     progress = models.IntegerField(default=0)
 
 
 class Task(models.Model):
     desc = models.CharField(max_length=200)
-    start_date = models.DateTimeField("date published")
-    status = models.CharField(max_length=1, choices=STATUSES)
+    proejct = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField("date created")
+    updated_at = models.DateTimeField("date updated")
+    status = models.CharField(max_length=20, choices=STATUSES)
     summary = models.CharField(max_length=10)
     progress = models.IntegerField(default=0)
 
 
 class Asignee(models.Model):
     name = models.CharField(max_length=200)
+    created_at = models.DateTimeField("date created")
+    updated_at = models.DateTimeField("date updated")
+    manager = models.ForeignKey("self", on_delete=models.DO_NOTHING, blank=True, null=True, default=None)
